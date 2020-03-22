06d27b555521ffb247e39b01051f4bbb8893c1bf
awalone <migoyeng@gmail.com>
2020-03-22 13:39:47 +0800

    belajar object prototype

--- script.js
+++ script.js
@@ -12,44 +12,65 @@
 
 // 2. function declaration
 // problem : tidak efektif dalam memori
-// function Mahasiswa(nama, energi) {
-//     let mahasiswa = {};
-//     mahasiswa.nama = nama;
-//     mahasiswa.energi = energi;
+const methodMahasiswa = {
+    makan : function (porsi) {
+        this.energi += porsi;
+        console.log(`Halo ${this.nama}, selamat makan`);
+    },
 
-//     mahasiswa.makan = function (porsi) {
-//         this.energi += porsi;
-//         console.log(`Hallo ${this.nama}, selamat Makan`);
-//     }
+    main : function (jam) {
+        this.energi -= jam;
+        console.log(`Halo ${this.nama}, selamat bermain`);
+    },
 
-//     mahasiswa.main = function (jam) {
-//         this.energi -= jam;
-//         console.log(`Halo ${this.nama}, selamat bermain`);
-//     }
+    tidur : function(jam) {
+        this.energi += jam * 2;
+        console.log(`Halo ${this.nama}, selamat tidur`);
+    }
+};
 
-//     return mahasiswa;
-// }
+function Mahasiswa(nama, energi) {
+    let mahasiswa = {methodMahasiswa};
+    mahasiswa.nama = nama;
+    mahasiswa.energi = energi;
+    
+    // mahasiswa.makan = function (porsi) {
+    //     this.energi += porsi;
+    //     console.log(`Hallo ${this.nama}, selamat Makan`);
+    // }
 
-// let awalone = Mahasiswa('Awalone', 10);
-// let rayyan = Mahasiswa('Ar Rayyan', 20);
+    // mahasiswa.main = function (jam) {
+    //     this.energi -= jam;
+    //     console.log(`Halo ${this.nama}, selamat bermain`);
+    // }
+
+
+    // mahasiswa.makan = methodMahasiswa.makan;
+    // mahasiswa.main = methodMahasiswa.main;
+    // mahasiswa.tidur = methodMahasiswa.tidur;
+    return mahasiswa;
+}
+
+let awalone = Mahasiswa('Awalone', 10);
+let rayyan = Mahasiswa('Ar Rayyan', 20);
 
 // tes aja nih
 
 // 3. constuctor function
-function Mahasiswa(nama, energi) {
-    this.nama = nama;
-    this.energi = energi;
+// function Mahasiswa(nama, energi) {
+//     this.nama = nama;
+//     this.energi = energi;
 
-    this.makan = function (porsi) {
-        this.energi += porsi;
-        console.log(`Halo ${this.nama}, selamat makan`);
-    }
+//     this.makan = function (porsi) {
+//         this.energi += porsi;
+//         console.log(`Halo ${this.nama}, selamat makan`);
+//     }
 
-    this.main = function(jam) {
-        this.energi -= jam;
-        console.log(`Halo ${this.nama}, selamat bermain`);
-    }
-}
+//     this.main = function(jam) {
+//         this.energi -= jam;
+//         console.log(`Halo ${this.nama}, selamat bermain`);
+//     }
+// }
 
-let awal = new Mahasiswa('awalone', 20);
+// let awal = new Mahasiswa('awalone', 20);
 // 4 object.create
